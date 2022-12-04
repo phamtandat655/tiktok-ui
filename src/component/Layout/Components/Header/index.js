d2f012699bdf43310191e7a7308a7856bd060de8
TanDat <phamtandat655@gmail.com>
2022-12-04 10:17:21 +0700

    Sửa lỗi và hoàn thiện phần Header #1

--- src/component/Layout/Components/Header/index.js
+++ src/component/Layout/Components/Header/index.js
@@ -14,7 +14,9 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
 
+import routesConfig from '~/config/routes';
 import Button from '~/component/Button';
 import Menu from '~/component/Popper/Menu';
 import { InboxIcon, MessageIcon, UploadIcon } from '~/component/Icons';
@@ -94,7 +96,9 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <img src={images.logo} alt="tiktok" />
+                <Link to={routesConfig.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="tiktok" />
+                </Link>
 
                 {/* Search */}
                 <Search />
---
+++ src/config/routes.js
@@ -0,0 +1,10 @@
+// dùng để define những tuyến đường
+const routes = {
+    home: '/',
+    following: '/following',
+    profile: '/@:nickname',
+    upload: '/upload',
+    search: '/search',
+};
+
+export default routes;
